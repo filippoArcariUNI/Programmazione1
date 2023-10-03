6ed1fe32a89b5430afc62b3654010b60902aa74c
filippoArcariUNI <146063416+filippoArcariUNI@users.noreply.github.com>
2023-10-03 19:52:16 +0000

    es03-10

--- esercizi03-10/es5.cc
+++ esercizi03-10/es5.cc
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main(){
+    int h;
+    cout  << "Inserisci l' altezza del triangolo "<<endl;
+    cin >> h;
+    for (int i = 0; i < h  ; i++)
+    {
+        
+        
+        for (int j = 0; j < h; j++)
+        {
+            if (j<(h+1)/2 || j>(h+1)/2)
+            {
+            cout << " ";
+            }else{
+            cout << "*";
+            }
+        }
+        cout << endl;
+        
+    }
     
 }
