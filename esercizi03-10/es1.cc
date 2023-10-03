1c2000aabe02044fa956a0505f0913d98b9d2e70
filippoArcariUNI <146063416+filippoArcariUNI@users.noreply.github.com>
2023-10-03 14:18:40 +0000

    es03-10

--- esercizi03-10/es1.cc
+++ esercizi03-10/es1.cc
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main(){
-    int n,npre=0,npre2=1,nTemp;
-    int risultato=0;
+    int n,npre=0,npre2=1;
+    int risultato=2;
+    bool cambio;
     cout << "Inserisci un numero" << endl;
     cin >> n;
 
@@ -12,12 +13,24 @@ int main(){
         if (i==1)
         {
           cout << npre<< endl;
+          cout << npre2<< endl;
+          cout << npre2<< endl;
+          npre =npre2;
+          cout << endl ;
+
         }else
         {
-            
+          risultato=npre+npre2;
+          if (cambio)
+          {
+            npre=risultato;
+            cambio=false;
+          }else{
+             npre2=risultato;
+            cambio=true;
+          }
+          cout << risultato << endl;
         }
-
-       
         
     }
     return 0;
