b11fdf43232d1e66dc16471195a5a2f83adb7b18
filippoArcariUNI <filippo.arcari@studenti.unitn.it>
2023-10-12 12:18:05 +0200

    es10-12

--- esercizi10-05/es3.cc
+++ esercizi10-05/es3.cc
@@ -3,21 +3,21 @@ using namespace std;
 
 bool isPrime(int n){
     int counter=0;
- if ((n%2)==0)
+ if ((n%2==0 && n!=2))
  {
     counter++;
- }else if ((n%3)==0)
+ }else if ((n%3==0 && n!=3))
  {
     counter++;
- }else if ((n%5)==0)
- {
-    counter++;
- }
- else if ((n%7)==0)
+ }else if ((n%5==0 && n!=5))
  {
     counter++;
  }
- else if ((n%11)==0){
+ else if ((n%7==0 && n!=7))
+ {
+    counter++;
+ }
+ else if ((n%11==0 && n!=11)){
     counter++;
  }
  if (counter<1){
@@ -30,21 +30,28 @@ bool isPrime(int n){
 }
 
 int main(){
-    int inpt;
+    int inpt,i;
     int rimanenza;
     cout << "Insericsci un numero"<< endl;
     cin >> inpt;
 rimanenza=inpt;
-cout << inpt;
-    for (int i = rimanenza; i > 0 && rimanenza>0; i--)
-    {
-       if (isPrime(i))
+cout << inpt<< "=>";
+
+      i=rimanenza;
+   while (i>0)
+   {
+      if (isPrime(i))
        {
-       cout << "=> :" << i;
+       cout << i;
         rimanenza-=i;
-        i=rimanenza;
+        i=rimanenza+1;
+       if(i!=1){
+         cout << "+";
        }
-    }
+       }
+       i--;
+   }
+   
     
 
     return 0;
