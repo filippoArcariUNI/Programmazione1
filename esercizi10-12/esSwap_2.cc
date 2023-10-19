0e7b1779ae60cc962c5242f8e84aa9b0673cceb8
filippoArcariUNI <146063416+filippoArcariUNI@users.noreply.github.com>
2023-10-19 20:15:09 +0000

    completamentoEsercizi

---
+++ esercizi10-12/esSwap_2.cc
@@ -0,0 +1,9 @@
+// Dati in input due interi (short), scrivere un programma che
+// con una procedura scambi gli 8 bit meno significativi dei
+// due numeri.
+// Aggiuntivo - Swap v2 - Difficile
+// 24
+// 1855(0000011100111111)
+// 1048(0000010000011000) =>
+// 1816(0000011100011000)
+// 1087(0000010000111111)
