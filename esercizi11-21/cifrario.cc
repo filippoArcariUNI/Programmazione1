a1bef23becc4d7e94575bf248d460d28290c5f25
filippoArcariUNI <146063416+filippoArcariUNI@users.noreply.github.com>
2023-11-22 18:03:45 +0000

    cifrario

--- esercizi11-21/cifrario.cc
+++ esercizi11-21/cifrario.cc
@@ -8,7 +8,7 @@ void crypt(char * str,int n){
         if (isalpha(str[i]))
         {
             
-        str[i] += -n + 'a' ;
+        str[i] += - 'a'+n ;
         str[i] %= ('z'-'a');
         str[i] +='a'-1;
         
@@ -18,16 +18,14 @@ void crypt(char * str,int n){
     } while (str[i]!='\0'); 
 
 }
-void deCrypt(char * str,int n){
-    
-    int i=0;
-    
-    
+void deCrypt(char * str,int n){   
+    int i=0; 
+  
     do
     {
         if (isalpha(str[i]))
         {
-        str[i] += -n + 'a' ;
+        str[i] += -'a' -n;
         str[i] %= ('z'-'a');
         str[i] +='a'-1;
         
