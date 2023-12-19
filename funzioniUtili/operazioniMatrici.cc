c25bb298b335a0bd5a2cbbfca32157f4cd7a0c88
filippoArcariUNI <filippo.arcari@studenti.unitn.it>
2023-12-19 10:18:46 +0100

    simEsame19-12

--- funzioniUtili/operazioniMatrici.cc
+++ funzioniUtili/operazioniMatrici.cc
@@ -31,3 +31,65 @@ void RiempiMatrice(int  matrix[righe][colonne]){
     }
     
 }
+
+void trasponiMatrice(int ** matrix,int ** newMatrix,int righe,int colonne){
+    for (int i = 0; i < righe; i++)
+    {
+        for (int  j = 0; j < colonne; j++)
+        {
+            newMatrix[i][j]=matrix[i][j];
+        }
+        
+    }
+}
+
+void stampaMatrice(int **matrix,int righe, int colonne){
+    for (int i = 0; i < righe; i++)
+    {
+        for (int  j = 0; j < colonne; j++)
+        {
+           cout << matrix[i][j]<< " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+void stampasottoMatrice(int  ** matrix,int righe ,int colonne, int x,int y){
+
+    for (int i = x; i <= righe; i++)
+    {
+        for (int  j = y; j <= colonne; j++)
+        {
+           cout << matrix[i][j]<< " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+void RiempiMatrice(int **matrix,int righe, int colonne){
+    for (int i = 0; i < righe; i++)
+    {
+        for (int  j = 0; j < colonne; j++)
+        {
+            matrix[i][j]=rand()%10;
+        }
+        
+    }
+    
+}
+
+int ** creaMatriceDinamica(int righe,int colonne){
+    int ** matrice =new int*[righe];
+    for (int  i = 0; i < righe; i++)
+    {
+        matrice[i] =new int[colonne];
+    }
+    return matrice;  
+}
+void deallocMatrix(int ** matrix, int rows){
+    for (int i = 0; i < rows; ++i)
+    delete [] matrix[i];
+    delete [] matrix;
+}
---
+++ simEsame19-12-2023/es1.cc
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+void toLower(char * str){
+    for ( int  i = 0; str[i]!= '\0'; i++)
+    {
+        if (str[i]>='A' && str[i]<='Z')
+        {   
+            str[i]-='A'-'a';
+        }
+    }
+    
+}
+char * circularShift(char * str, int shift){
+    
+    int dim=0;
+    for (int  i = 0; str[i]!='\0'; i++)
+    {
+        dim++;
+    }
+    char * temp=new char[dim];
+    for (int  i = 0; i<dim; i++)
+    {
+        int index=(dim-1-i)%dim;
+    
+        temp[i]=str[index];
+        
+    }
+
+    return temp;
+}
+
+char  ** creaMatriceDinamica(int righe,int colonne){
+    char ** matrice =new char*[righe];
+    for (int  i = 0; i < righe; i++)
+    {
+        matrice[i] =new char[colonne];
+    }
+    return matrice;  
+}
+void deallocMatrix(char ** matrix, int rows){
+    for (int i = 0; i < rows; ++i)
+    delete [] matrix[i];
+    delete [] matrix;
+}
+
+int contaOccorrenza(char ** matrix, char * cercata,int righe){
+    int counter=0;
+    int dimPattern=strlen(cercata);
+    for (int  i = 0; i < righe; i++)
+    {
+        int dimColonna=strlen(matrix[i]);
+        int k=0;
+        for (int  j = 0; j < dimColonna; j++)
+        {
+            cout << matrix[i][j]<<cercata[k]<<  endl;
+            
+            if (matrix[i][j]==cercata[k])
+            {
+                k++;
+            }else{
+                k=0;
+            }
+            if (k==dimPattern)
+            {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+int main(int nArg,char * arg[]){
+     if (nArg != 2)
+    {
+        cout << "Usage: ./a.out <fileInput>  \n";
+        exit(1);
+    }
+    fstream in,out,contatore;
+    in.open(arg[1],ios::in);
+    contatore.open(arg[1],ios::in);
+
+    if (in.fail() || contatore.fail())
+    {
+        cout << "Errore nell' apertura dei file \n";
+        exit(2);
+    }
+    
+    char str[256];
+    int nStringhe=0;
+    
+    while (contatore>> str)
+    {
+        nStringhe++;
+    }
+   
+    char ** matrice=creaMatriceDinamica(nStringhe,256);
+    int i=0;
+    while (in>> str)
+    {
+        strcpy(matrice[i],str);
+        i++;
+    }
+
+    int shift;
+    char cercata[256];
+    cout << "Inserisci una Parola e il suo shift " << endl;
+    cin >> cercata >> shift;
+
+    toLower(cercata);
+    cout << cercata<< endl;
+
+    char * temp=circularShift(cercata,shift);
+    cout << temp<< " <--shift "<<endl;
+
+    int occorrenza = contaOccorrenza(matrice,temp,nStringhe);
+    cout << occorrenza<< endl;
+    
+    deallocMatrix(matrice,nStringhe);
+    delete[] temp;
+    in.close();
+    contatore.close();
+    out.close();
+}
